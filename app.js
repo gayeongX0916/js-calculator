d31f7d243964c7400519f606f712b4e0aef190a5
gayeongX0916 <gayeongx0916@gmail.com>
2025-05-16 15:47:12 +0900

    [feature] 주석 추가

--- app.js
+++ app.js
@@ -15,14 +15,9 @@ buttons.forEach((btn) => {
       displayInput.value.lastIndexOf("*"),
       displayInput.value.lastIndexOf("/")
     );
-    const lastNum = displayInput.value.slice(lastOperatorIndex + 1);
+    let lastNum = displayInput.value.slice(lastOperatorIndex + 1);
 
-    if (value === "AC") {
-      resultInput.value = "";
-      displayInput.value = "";
-      return;
-    }
-
+    // 계산 결과 출력
     if (value === "=") {
       try {
         resultInput.value = eval(displayInput.value);
@@ -32,24 +27,34 @@ buttons.forEach((btn) => {
       return;
     }
 
+    // AC 기능
+    if (value === "AC") {
+      displayInput.value = "";
+      resultInput.value = "";
+      return;
+    }
+
+    // 연산자 먼저 입력 방지
     if (displayInput.value === "" && operators.includes(value)) {
       return;
     }
 
+    // 소수점 중복 입력 방지
     if (value === ".") {
       if (lastNum.includes(".")) return;
     }
 
+    // 0으로 시작하는 숫자 입력 제한
+    if (!isNaN(value) && displayInput.value) {
+      if (lastNum.startsWith(0) && value !== "." && !lastNum.includes("."))
+        return;
+    }
+
+    // 연산자 연속 입력시 마지막 연산자로 자동 대체
     const lastChar = displayInput.value.slice(-1);
     if (operators.includes(lastChar) && operators.includes(value)) {
       displayInput.value = displayInput.value.slice(0, -1) + value;
       return;
-    }
-
-    if (!isNaN(value) && displayInput.value) {
-      if (lastNum.startsWith("0") && !lastNum.includes(".") && value !== ".") {
-        return;
-      }
     }
 
     displayInput.value += value;
